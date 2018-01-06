4276780932992de7351076d539ab3dd9fd6a36f4
imikutsk <ira_stan@mail.ru>
2018-01-06 23:34:18 +0100

    wyszukiwanie linkow stron konferencji

--- find_subpages.py
+++ find_subpages.py
@@ -44,6 +44,7 @@ def conf_link():
         #url='http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71733&copyownerid=13881'
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser', parse_only=SoupStrainer('a'))
+        #soup = BeautifulSoup(r.content, 'html.parser').find('td', align_='center')
         #print([link['href'] for link in soup if link.has_attr('href')])
         print("linkow na stronie jest:", len(soup.find_all('a', href=True)))
         ii=0
@@ -58,4 +59,49 @@ def conf_link():
         print("link_home = ", link_home)
 
 
-conf_link()
+#conf_link()
+
+
+# td align="center"
+
+def ww():
+    print("---------test3 page 2---------")
+
+    import urllib.request
+    from bs4 import BeautifulSoup
+
+    link_home0=0
+    html = urllib.request.urlopen('http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71777&copyownerid=89732')
+    soup = BeautifulSoup(html, 'html.parser').find('div', class_='contsec')
+    print("len", len(soup.find_all('a', href=True)))
+    count=0
+    for i in soup.find_all('a', href=True):
+        #if not i.startswith('/') and not i.startswith('tel:'):
+        count=count+1
+        #if count<=20:
+        print(i['href'])
+        link_home0=link_home0+1
+    print(link_home0)
+
+
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
+#import lxml
+
+FORBIDDEN_PREFIXES = ['/', 'tel:', 'mailto:', '.']
+
+url='http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71777&copyownerid=89732'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser', parse_only=SoupStrainer('a'))
+link_home0 = 0
+for i in soup.find_all('a'):
+    link = i['href']
+    #if not link.startswith('/'):
+    if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
+    #if not link.startswith()
+        print(i['href'])
+        link_home0 = link_home0 + 1
+print(link_home0)
+
+
