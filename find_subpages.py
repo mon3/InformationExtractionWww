e6850873b6190f07f2e9a7023cb92fa1e4d1e9cf
imikutsk <ira_stan@mail.ru>
2018-01-07 16:03:32 +0100

    wyszukiwanie konferencji na wikicfp, zapisywanie linkow (podstron wikicfp) do pliku tekstowego wikicfp_conf.txt, wyszukiwanie na tych podstronach linkow do stron konferencji i zapisywanie ich do pliku conf.txt

--- find_subpages.py
+++ find_subpages.py
@@ -3,105 +3,52 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
 
-def wikicfp_link(number):
-    #link_home0=0
-    #link='http://www.wikicfp.com/cfp/allcfp?page=2'
+def wikicfp_link(number, n2):
     link='http://www.wikicfp.com/cfp/allcfp?page='
-    #number = 3 #indeks strony na wikicfp (ile stron musimu przeszukac)
     ind=1
-
     f = open('wikicfp_conf.txt', 'w')
-
-    while ind <= number:
-        html = urllib.request.urlopen(link + str(ind))
+    #while ind <= number:
+    #while number!=0:
+    while number >= n2:
+        html = urllib.request.urlopen(link + str(number))
+        #html = urllib.request.urlopen(link + str(ind))
         soup = BeautifulSoup(html, 'html.parser').find('div', class_='contsec')
-        #print("len", len(soup.find_all('a', href=True)))
         count=0
-        #f = open('wikicfp_conf.txt', 'w')
         for i in soup.find_all('a', href=True):
             count=count+1
             if count<=20: # na kazdej stronie jest 20 linkow na konferencje
                 f.write("http://www.wikicfp.com" +i['href'] + '\n')
                 print("http://www.wikicfp.com" +i['href'])
-                #link_home0=link_home0+1
-        ind=ind+1
+        number=number-1
     f.close()
-    #print(link_home0)
 
-
-
-
-
-#wikicfp_link(1)
-
-
-
-def conf_link():
-    myfile = open("wikicfp_conf.txt", "r")  # чтение из файла
-    for line in myfile.readlines():  # построчно читаем файл и выводим на экран
+def conf_link2():
+    myfile = open("wikicfp_conf.txt", "r")
+    FORBIDDEN_PREFIXES = ['/', 'tel:', 'mailto:', '.', 'http://www.facebook.com', 'http://twitter.com', 'http://www.linkedin.com', 'https://plus.google.com']
+    f = open('conf.txt', 'w')
+    for line in myfile.readlines():
         # print (line)
+        link_home0 = 0
         url = line.strip()
-        #url='http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71733&copyownerid=13881'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html.parser', parse_only=SoupStrainer('a'))
-        #soup = BeautifulSoup(r.content, 'html.parser').find('td', align_='center')
-        #print([link['href'] for link in soup if link.has_attr('href')])
-        print("linkow na stronie jest:", len(soup.find_all('a', href=True)))
-        ii=0
-        link_home=0
-        for link in soup:
-            ii = ii + 1
-            if ii==25 :
-                print("link strony konferencji: ", link['href'])
-                link_home=link_home+1
-
-        #print(ii)
-        print("link_home = ", link_home)
-
-
-#conf_link()
+        html = urllib.request.urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser').find('div', class_='contsec')
+        #print("---------")
+        #print("linkow na stronie jest:", len(soup.find_all('a', href=True)))
+        for i in soup.find_all('a'):
+            link = i['href']
+            if link.startswith('http') & (all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES)):
+                link_home0 = link_home0 + 1
+                #print(i['href'])
+                if link_home0==1:
+                    f.write(i['href'] + '\n')
+                    print("link strony konferencji: ", i['href'])
+        #print("link_home0", link_home0)
+    f.close()
+    myfile.close()
 
 
-# td align="center"
-
-def ww():
-    print("---------test3 page 2---------")
-
-    import urllib.request
-    from bs4 import BeautifulSoup
-
-    link_home0=0
-    html = urllib.request.urlopen('http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71777&copyownerid=89732')
-    soup = BeautifulSoup(html, 'html.parser').find('div', class_='contsec')
-    print("len", len(soup.find_all('a', href=True)))
-    count=0
-    for i in soup.find_all('a', href=True):
-        #if not i.startswith('/') and not i.startswith('tel:'):
-        count=count+1
-        #if count<=20:
-        print(i['href'])
-        link_home0=link_home0+1
-    print(link_home0)
 
 
-from urllib.parse import urlparse
-from bs4 import BeautifulSoup
-import requests
-#import lxml
-
-FORBIDDEN_PREFIXES = ['/', 'tel:', 'mailto:', '.']
-
-url='http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=71777&copyownerid=89732'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser', parse_only=SoupStrainer('a'))
-link_home0 = 0
-for i in soup.find_all('a'):
-    link = i['href']
-    #if not link.startswith('/'):
-    if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
-    #if not link.startswith()
-        print(i['href'])
-        link_home0 = link_home0 + 1
-print(link_home0)
-
+wikicfp_link(5, 5)
+conf_link2()
 
