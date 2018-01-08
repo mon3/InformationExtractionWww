d0d153d0d609c9c12760b4c01953d9a525accdbf
mon3 <seniut.monika@gmail.com>
2018-01-09 00:18:16 +0100

    [WIP] CRF model implementation

--- training_set_preparation.py
+++ training_set_preparation.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 from bs4.element import Tag
+from nltk import pos_tag
+from sklearn.model_selection import train_test_split
+import pycrfsuite
 
 def get_text_and_tags_recursive(text, textTags, currentSoup, currentTags):
     if type(currentSoup) == NavigableString:
@@ -53,17 +56,156 @@ def get_classificated_text(html):
     # return single_words, classes
     return docs
 
+def word2features(doc, i):
+    word = doc[i][0]
+    postag = doc[i][1]
+
+    # Common features for all words
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word.isupper=%s' % word.isupper(),
+        'word.istitle=%s' % word.istitle(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag
+    ]
+
+    # Features for words that are not
+    # at the beginning of a document
+    if i > 0:
+        word1 = doc[i-1][0]
+        postag1 = doc[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:word.isdigit=%s' % word1.isdigit(),
+            '-1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'beginning of a document'
+        features.append('BOS')
+
+    # Features for words that are not
+    # at the end of a document
+    if i < len(doc)-1:
+        word1 = doc[i+1][0]
+        postag1 = doc[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:word.isdigit=%s' % word1.isdigit(),
+            '+1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'end of a document'
+        features.append('EOS')
+
+    return features
+
+# A function for extracting features in documents
+def extract_features(doc):
+    return [word2features(doc, i) for i in range(len(doc))]
+
+# A function fo generating the list of labels for each document
+def get_labels(doc):
+    return [label for (token, postag, label) in doc]
+
+def prepare_crf_trainer(X_train, Y_train):
+    trainer = pycrfsuite.Trainer(verbose=False)  # verbose = TRue wypisze
+    # progres trenowania
+
+    # Submit training data to the trainer
+    for xseq, yseq in zip(X_train, Y_train):
+        trainer.append(xseq, yseq)
+
+    # Set the parameters of the model
+    trainer.set_params({
+        # coefficient for L1 penalty
+        'c1': 0.1,
+
+        # coefficient for L2 penalty
+        'c2': 0.01,
+
+        # maximum number of iterations
+        'max_iterations': 200,
+
+        # whether to include transitions that
+        # are possible, but not observed
+        'feature.possible_transitions': True
+    })
+
+    # Provide a file name as a parameter to the train function, such that
+    # the model will be saved to the file when training is finished
+    trainer.train('crf.model')
+
+def get_files(directory):
+    import os
+
+    html_files = []
+    for dir in os.listdir(directory):
+        if (dir.endswith(".txt")==0):
+            for file in os.listdir(os.path.join(directory, dir)):
+                if file.endswith(".html"):
+                    html = open(os.path.join(os.path.join(directory, dir), file),'r').read()
+                    html_files.append(html)
+            # print(html_files[1])
+    return html_files
+
+if __name__== "__main__":
+
+    html_files = get_files("conferences-data/pagestorage-annotated/")
+    # import os
+    # print(os.listdir("conferences-data/pagestorage-annotated/0/"))
+    # html = open('conferences-data/pagestorage-annotated/0/1.html', 'r').read()
+    # print(html)
+    # words, classes = get_classificated_text(html)
 
 
-html = open('conferences-data/pagestorage-annotated/0/1.html', 'r').read()
-# words, classes = get_classificated_text(html)
+    # for word, word_class in zip(words, classes):
+    #     if word_class != 'DIFF':
+    #         print (word, word_class)
+    docs = []
+    for i in range(len(html_files)):
+        doc = get_classificated_text(html_files[i])
+        docs.append(doc)
+    # for class_word in docs:
+    #     for i in class_word:
+    #         print(i)
+
+    data = []
+    for j in docs:
+        for i, doc in enumerate(j):
+            # Obtain the list of tokens in the document
+            tokens = [t for t, label in doc]
+
+            # Perform POS tagging
+            tagged = pos_tag(tokens)
+
+            # Take the word, POS tag, and its label
+            data.append(
+                [(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
+
+    # print(data)
+
+    X = [extract_features(doc) for doc in data]
+    y = [get_labels(doc) for doc in data]
 
 
-# for word, word_class in zip(words, classes):
-#     if word_class != 'DIFF':
-#         print (word, word_class)
 
-docs = get_classificated_text(html)
-for class_word in docs:
-    for i in class_word:
-        print(i)
+    # # print(X, y)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+    prepare_crf_trainer(X_train, Y_train)
+
+    tagger = pycrfsuite.Tagger()
+    tagger.open('crf.model')
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
+    # print(y_pred[-1], X_test[-1])
+
+    # Let's take a look at a random sample in the testing set
+    # i = 100
+    for x, y in zip(y_pred[i], [x[1].split("=")[1] for x in X_test[i]]):
+        print("%s (%s)" % (y, x))
